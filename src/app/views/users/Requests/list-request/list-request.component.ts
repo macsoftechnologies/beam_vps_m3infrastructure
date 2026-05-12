d082ed493adacb50589dca765cfa833e56246bae
macsoftechnologies@gmail.com <macsoftechnologies@gmail.com>
2026-05-12 18:15:58 +0530

    copy request modified for zone issue

--- src/app/views/users/Requests/list-request/list-request.component.ts
+++ src/app/views/users/Requests/list-request/list-request.component.ts
@@ -1245,7 +1245,7 @@ const matchedBuilding = buildingData.find(
 const matchedZones =
   matchedBuilding?.zoneList
     ?.filter((zone) =>
-      zone.zoneSubList.some((sub) => roomNosArray.includes(sub.value))
+      zone.zoneSubList.some((sub) => roomNosArray.includes(sub.value.trim()))
     )
     .map((zone) => zone.floorName) || [];
 
